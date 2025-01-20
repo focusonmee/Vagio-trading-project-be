2f32158778479ffd6db0ccf55187428dbd929bf0
focus_On_Huy <pleasestayfocus@gmail.com>
2025-01-20 22:53:30 +0700

    first commit

---
+++ src/main/java/com/example/VagioTradingApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagioTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagioTradingApplication.class, args);
+	}
+
+}
